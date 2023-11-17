080623ded896106a1a8e37f5176bd751e018947a
minetoo3405 <analog3405@gmail.com>
2023-11-17 20:43:59 +0900

    중복 데이터 수정 및 코드 관리. 디버깅 추가

--- dataset/train_set.c
+++ dataset/train_set.c
@@ -1,40 +1,36 @@
 #include <stdint.h>
 
 // 'square (13)', 8x8px
-const uint8_t v$dataset$train_datasquare__13_ [] = {
+const uint8_t v$dataset$train_datasquare__12_ [] = {
 	0xff, 0xff, 0xff, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0
 };
 // 'square (12)', 8x8px
-const uint8_t v$dataset$train_datasquare__12_ [] = {
+const uint8_t v$dataset$train_datasquare__11_ [] = {
 	0x83, 0x83, 0x83, 0x83, 0x83, 0xff, 0xff, 0xff
 };
 // 'square (11)', 8x8px
-const uint8_t v$dataset$train_datasquare__11_ [] = {
+const uint8_t v$dataset$train_datasquare__10_ [] = {
 	0xff, 0xff, 0xc3, 0xc3, 0xc3, 0xc3, 0xff, 0xff
 };
 // 'square (10)', 8x8px
-const uint8_t v$dataset$train_datasquare__10_ [] = {
+const uint8_t v$dataset$train_datasquare__9_ [] = {
 	0xff, 0xff, 0x07, 0x07, 0x07, 0x07, 0x07, 0xff
 };
 // 'square (9)', 8x8px
-const uint8_t v$dataset$train_datasquare__9_ [] = {
+const uint8_t v$dataset$train_datasquare__8_ [] = {
 	0xff, 0xff, 0xff, 0xff, 0x83, 0x83, 0x83, 0x83
 };
 // 'square (8)', 8x8px
-const uint8_t v$dataset$train_datasquare__8_ [] = {
+const uint8_t v$dataset$train_datasquare__7_ [] = {
 	0xff, 0xff, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0
 };
 // 'square (7)', 8x8px
-const uint8_t v$dataset$train_datasquare__7_ [] = {
+const uint8_t v$dataset$train_datasquare__6_ [] = {
 	0x83, 0x83, 0x83, 0x83, 0x83, 0xff, 0xff, 0xff
 };
 // 'square (6)', 8x8px
-const uint8_t v$dataset$train_datasquare__6_ [] = {
-	0xff, 0x07, 0x07, 0x07, 0x07, 0x07, 0xff, 0xff
-};
-// 'square (5)', 8x8px
 const uint8_t v$dataset$train_datasquare__5_ [] = {
-	0xff, 0xff, 0xff, 0xff, 0xf0, 0xf0, 0xf0, 0xf0
+	0xff, 0x07, 0x07, 0x07, 0x07, 0x07, 0xff, 0xff
 };
 // 'square (4)', 8x8px
 const uint8_t v$dataset$train_datasquare__4_ [] = {
@@ -55,33 +51,25 @@ const uint8_t v$dataset$train_datasquare [] = {
 
 
 // 'triangle (13)', 8x8px
-const uint8_t v$dataset$train_datatriangle__13_ [] = {
+const uint8_t v$dataset$train_datatriangle__11_ [] = {
 	0xff, 0xff, 0xff, 0xcf, 0xcf, 0x87, 0x87, 0x03
 };
 // 'triangle (12)', 8x8px
-const uint8_t v$dataset$train_datatriangle__12_ [] = {
+const uint8_t v$dataset$train_datatriangle__10_ [] = {
 	0xff, 0xff, 0xcf, 0x87, 0x87, 0x03, 0x03, 0x01
 };
 // 'triangle (11)', 8x8px
-const uint8_t v$dataset$train_datatriangle__11_ [] = {
+const uint8_t v$dataset$train_datatriangle__9_ [] = {
 	0xff, 0xff, 0xff, 0xfb, 0xf9, 0xf1, 0xf0, 0xe0
 };
 // 'triangle (10)', 8x8px
-const uint8_t v$dataset$train_datatriangle__10_ [] = {
+const uint8_t v$dataset$train_datatriangle__8_ [] = {
 	0xff, 0xff, 0xff, 0x9f, 0x8f, 0x0f, 0x07, 0x07
 };
-// 'triangle (9)', 8x8px
-// const uint8_t v$dataset$train_datatriangle__9_ [] = {
-// 	0xff, 0xff, 0xff, 0xfd, 0xf9, 0xf8, 0xf0, 0xf0
-// };
 // 'triangle (8)', 8x8px
-const uint8_t v$dataset$train_datatriangle__8_ [] = {
+const uint8_t v$dataset$train_datatriangle__7_ [] = {
 	0xff, 0xff, 0xfb, 0xf3, 0xe1, 0xe1, 0xc0, 0xc0
 };
-// 'triangle (7)', 8x8px
-// const uint8_t v$dataset$train_datatriangle__7_ [] = {
-// 	0xff, 0xff, 0xff, 0xff, 0xfd, 0xf8, 0xf0, 0xf0
-// };
 // 'triangle (6)', 8x8px
 const uint8_t v$dataset$train_datatriangle__6_ [] = {
 	0xff, 0xff, 0xff, 0xff, 0xfb, 0xfb, 0xf1, 0xe1
--- main.c
+++ main.c
@@ -3,56 +3,6 @@
 #include <time.h>
 
 
-int XXmain( int argc, char* argv[] )
-{
-  clock_t start_time = 0;
-  clock_t end_time = 0;
-  int64_t result = 0;
-
-  printf("\n시작.");
-  start_time = clock();
-
-
-  uint64_t input_range[] = { 14 };
-  t$pctr$RawData input_data = f$pctr$newRawData(e$pctr$Size$bit_8, 6);
-  f$pctr$RawData$set(input_data, 0, 1);
-  f$pctr$RawData$set(input_data, 1, 4);
-  f$pctr$RawData$set(input_data, 2, 8);
-  f$pctr$RawData$set(input_data, 3, 6);
-  f$pctr$RawData$set(input_data, 4, 10);
-  f$pctr$RawData$set(input_data, 5, 13);
-  t$pctr$RawData target_data = f$pctr$newRawData(e$pctr$Size$bit_8, 6);
-  f$pctr$RawData$set(target_data, 0, 0);
-  f$pctr$RawData$set(target_data, 1, 0);
-  f$pctr$RawData$set(target_data, 2, 0);
-  f$pctr$RawData$set(target_data, 3, 1);
-  f$pctr$RawData$set(target_data, 4, 2);
-  f$pctr$RawData$set(target_data, 5, 2);
-  t$pctr$Model model = f$pctr$_newModel(
-    1, input_range,
-    0,0,
-    4,
-    0
-  );
-  f$pctr$Model$_fit(model, input_data, target_data, 1, 0);
-
-  end_time = clock();
-  printf("\n총 실행에 걸린 시간 : %ldms (%.3fs)", end_time-start_time, (end_time-start_time)/1000.0);
-
-  for ( int i=0; i<=13; i++ )
-  {
-    result = f$pctr$Model$calc(model, i);
-    printf("\n 모델 연산 | in:%d, out:%lld", i, result);
-  }
-
-  model = f$pctr$Model$release(model);
-
-  getchar();
-  return( 0 );
-}
-
-
-
 // -- 8*8 도형 이미지 분류 테스트.
 //? 레이블 당 8바이트.
 //? 사각형 : 0, 삼각형 : 1
@@ -60,30 +10,35 @@ int main()
 {
   // -- 학습 데이터 준비.
   const uint64_t v$dataset$train_input[] = {
-		*(const uint64_t*)v$dataset$train_datasquare,
-		*(const uint64_t*)v$dataset$train_datasquare__2_,
-		*(const uint64_t*)v$dataset$train_datasquare__3_,
-		*(const uint64_t*)v$dataset$train_datasquare__4_,
-		*(const uint64_t*)v$dataset$train_datasquare__5_,
-		*(const uint64_t*)v$dataset$train_datasquare__6_,
-		*(const uint64_t*)v$dataset$train_datasquare__7_,
-		*(const uint64_t*)v$dataset$train_datasquare__8_,
-		*(const uint64_t*)v$dataset$train_datasquare__9_,
-		*(const uint64_t*)v$dataset$train_datasquare__10_,
-		*(const uint64_t*)v$dataset$train_datatriangle,
-		*(const uint64_t*)v$dataset$train_datatriangle__2_,
-		*(const uint64_t*)v$dataset$train_datatriangle__3_,
-		*(const uint64_t*)v$dataset$train_datatriangle__4_,
-		*(const uint64_t*)v$dataset$train_datatriangle__5_,
-		*(const uint64_t*)v$dataset$train_datatriangle__6_,
-		*(const uint64_t*)v$dataset$train_datatriangle__8_,
-		*(const uint64_t*)v$dataset$train_datatriangle__10_,
-    *(const uint64_t*)v$dataset$train_datatriangle__11_,
-    *(const uint64_t*)v$dataset$train_datatriangle__12_,
+    
+    *(const uint64_t*)v$dataset$train_datasquare,
+    *(const uint64_t*)v$dataset$train_datasquare__2_,
+    *(const uint64_t*)v$dataset$train_datasquare__3_,
+    *(const uint64_t*)v$dataset$train_datasquare__4_,
+    *(const uint64_t*)v$dataset$train_datasquare__5_,
+    *(const uint64_t*)v$dataset$train_datasquare__6_,
+    *(const uint64_t*)v$dataset$train_datasquare__7_,
+    *(const uint64_t*)v$dataset$train_datasquare__8_,
+    *(const uint64_t*)v$dataset$train_datasquare__9_,
+    *(const uint64_t*)v$dataset$train_datasquare__10_,
+
+
+    *(const uint64_t*)v$dataset$train_datatriangle,
+    *(const uint64_t*)v$dataset$train_datatriangle__2_,
+    *(const uint64_t*)v$dataset$train_datatriangle__3_,
+    *(const uint64_t*)v$dataset$train_datatriangle__4_,
+    *(const uint64_t*)v$dataset$train_datatriangle__5_,
+    *(const uint64_t*)v$dataset$train_datatriangle__6_,
+    *(const uint64_t*)v$dataset$train_datatriangle__7_,
+    *(const uint64_t*)v$dataset$train_datatriangle__8_,
+    *(const uint64_t*)v$dataset$train_datatriangle__9_,
+    *(const uint64_t*)v$dataset$train_datatriangle__10_,
+
 	};
   t$pctr$RawData input_data = f$pctr$newRawData(e$pctr$Size$bit_16, v$dataset$train_data_arr_len);
   uint64_t max_data = 0;
   uint64_t min_data = 0ULL-1;
+
 
   //? 최소 최대값 구하기.
   for ( uint64_t i=0; i<v$dataset$train_data_arr_len; i++ )
@@ -93,10 +48,13 @@ int main()
   }
   printf("\n 최소 : %llu, 최대 : %llu, 차 : %llu", min_data, max_data, max_data-min_data);
 
-  //? 범위 변환.
-  for ( uint64_t i=0; i<v$dataset$train_data_arr_len; i++ )
-    f$pctr$RawData$set(input_data, i, f$pctr$convRange(min_data, max_data, v$dataset$train_input[i], 0, (1ULL<<15)-1));
 
+  //? 입력 범위 변환.
+  for ( uint64_t i=0; i<v$dataset$train_data_arr_len; i++ )
+    f$pctr$RawData$set(input_data, i, f$pctr$convRange(0, max_data, v$dataset$train_input[i], 0, (1ULL<<15)-1));
+
+
+  //? 변환된 값의 최소 최대값 구하기.
   max_data = 0;
   min_data = 0ull-1;
   for ( uint64_t i=0; i<v$dataset$train_data_arr_len; i++ )
@@ -106,6 +64,24 @@ int main()
   }
   printf("\n 최소 : %llu, 최대 : %llu", min_data, max_data);
 
+
+  //? 두 입력 데이터의 차 중 제일 작은 차 구하기.
+  uint64_t min_abs = 0ull-1;
+  for ( uint64_t i=0; i<v$dataset$train_data_arr_len-1; i++ )
+  {
+    uint64_t selected = v$dataset$train_input[i];
+    for ( uint64_t j=i+1; j<v$dataset$train_data_arr_len; j++ )
+    {
+      uint64_t calc = selected>v$dataset$train_input[j]?selected-v$dataset$train_input[j]:v$dataset$train_input[j]-selected;
+      // printf("\n 차 값 : %llu", calc);
+      if ( min_abs>calc ) min_abs = calc;
+      if ( calc==0 ) printf("\n 중복 값 인덱스 : %llu, %llu", i, j);
+    }
+  }
+  printf("\n 최소 차 : %llu", min_abs);
+
+
+  //? 정답 데이터 준비.
   const uint8_t v$dataset$train_target[] = {
     0,0,0,0,0,0,0,0,0,0,
 		1,1,1,1,1,1,1,1,1,1,
@@ -115,6 +91,8 @@ int main()
     f$pctr$RawData$set(target_data, i, v$dataset$train_target[i]);
   printf("\n 데이터 준비 됨.");
 
+
+  //? 입력, 정답 데이터 확인.
   for ( uint64_t i=0; i<v$dataset$train_data_arr_len; i++ )
   {
     printf("\n input : %llu, target : %d", (uint64_t)f$pctr$RawData$get(input_data, i), (int)f$pctr$RawData$get(target_data, i));
